e8b48ea24f27aeef8b480005322773b92227f9a6
nzo66 <nzo66.business@gmail.com>
2025-05-21 05:04:10 +0200

    listaMFP.py aktualisieren

--- listaMFP.py
+++ listaMFP.py
@@ -221,7 +221,7 @@ def eventi_m3u8_generator():
             if teams_match:
                 team1, team2 = teams_match.groups()
                 search_queries = [
-                    f"{team1} vs {team2} logo epg"
+                    f"{clean_event_name} logo epg"
                 ]
             else:
                 search_queries = [
