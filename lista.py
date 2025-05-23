d03d56dac34f14235e793d5feaa486d031b84971
nzo66 <nzo66.business@gmail.com>
2025-05-23 06:43:39 +0200

    lista.py aktualisieren

--- lista.py
+++ lista.py
@@ -280,8 +280,6 @@ def eventi_m3u8_generator_world():
             teams = None
             if " vs " in clean_event_name:
                 teams = clean_event_name.split(" vs ")
-            elif " - " in clean_event_name:
-                teams = clean_event_name.split(" - ")
             elif " VS " in clean_event_name:
                 teams = clean_event_name.split(" VS ")
             elif " VS. " in clean_event_name:
@@ -711,8 +709,6 @@ def eventi_m3u8_generator():
             teams = None
             if " vs " in clean_event_name:
                 teams = clean_event_name.split(" vs ")
-            elif " - " in clean_event_name:
-                teams = clean_event_name.split(" - ")
             elif " VS " in clean_event_name:
                 teams = clean_event_name.split(" VS ")
             elif " VS. " in clean_event_name:
